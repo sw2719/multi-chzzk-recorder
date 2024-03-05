bb70e35b145756a789f4f9936911ccabc44608e8
Shinwoo Kim <sw2719@naver.com>
2024-03-05 14:28:19 +0900

    API 요청에 Fake User-Agent 적용

--- chzzk/checker.py
+++ chzzk/checker.py
@@ -2,13 +2,17 @@ import requests
 import json
 
 from typing import Union, Dict
+from fake_useragent import UserAgent
+
+ua = UserAgent()
+request_header = {"User-Agent": ua.chrome}
 
 
 def get_channel_info(channel_id: str) -> Union[Dict, None]:
     """Get channel info from chzzk API.
     :param channel_id: Channel ID.
     :return: Channel info dict if channel exists, None otherwise."""
-    with requests.get(f'https://api.chzzk.naver.com/service/v1/channels/{channel_id}') as r:
+    with requests.get(f'https://api.chzzk.naver.com/service/v1/channels/{channel_id}', headers=request_header) as r:
         try:
             r.raise_for_status()
             data = json.loads(r.text)['content']
@@ -19,7 +23,7 @@ def get_channel_info(channel_id: str) -> Union[Dict, None]:
 
 
 def check_live(channel_id: str) -> (bool, dict | None):
-    with requests.get(f'https://api.chzzk.naver.com/service/v1/channels/{channel_id}/live-detail') as r:
+    with requests.get(f'https://api.chzzk.naver.com/service/v1/channels/{channel_id}/live-detail', headers=request_header) as r:
         try:
             r.raise_for_status()
         except requests.exceptions.HTTPError:
